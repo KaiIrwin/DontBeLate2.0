aaba1f6c8023f09f20a7dc0a9d78ba00fb3ca637
KaiIrwin <k.irwin@famitrtra.cjp>
2022-03-28 22:28:30 -0700

    designed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to:'home#index'
+  
+  get '/deal_input', to:'dealinput#new'
+  post '/deal_input', to:'regestration#create'
+
+  get '/sign_up', to:'registration#new'
+  post '/sign_up', to:'registration#create'
+
+  get '/sign_in', to:'registration#new'
+  post '/sign_in', to:'registration#create'
+
+  delete '/logout', to:'registration#create'
+
+  get '/result', to:'result#new'
 end
