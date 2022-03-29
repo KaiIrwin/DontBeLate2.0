d3c1afa292120ab4447ff8f2e07b14e96bc91fc6
KaiIrwin <k.irwin@famitrtra.cjp>
2022-03-29 03:07:22 -0700

    fixed route names

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   root to:'home#index'
   
   get '/deal_input', to:'dealinput#new'
-  post '/deal_input', to:'regestration#create'
+  post '/deal_input', to:'dealinput#create'
 
   get '/sign_up', to:'registration#new'
   post '/sign_up', to:'registration#create'
 
-  get '/sign_in', to:'registration#new'
-  post '/sign_in', to:'registration#create'
+  get '/sign_in', to:'session#new'
+  post '/sign_in', to:'session#create'
 
-  delete '/logout', to:'registration#create'
+  delete '/logout', to:'session#create'
 
   get '/result', to:'result#new'
 end
