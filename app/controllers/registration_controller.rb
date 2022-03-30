a00bac42dbce7a34c2accd8e5c67afcc9d6c7ff3
KaiIrwin <k.irwin@famitrtra.cjp>
2022-03-30 10:34:39 -0700

    sign up 続き

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -3,6 +3,14 @@ class RegistrationController < ApplicationController
     @user = User.new
   end
 
-  def create
+  def create 
+    @user = User.new(user_params)
+    render plain: '登録完了しました！'
   end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end 
 end
