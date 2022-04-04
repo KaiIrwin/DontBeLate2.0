0f70ba936f432cd484079715149483ca8d86202e
KaiIrwin <k.irwin@famitrtra.cjp>
2022-04-04 10:52:02 +0900

    Made dealinpout form and connected to database

--- app/controllers/dealinput_controller.rb
+++ app/controllers/dealinput_controller.rb
@@ -1,7 +1,19 @@
 class DealinputController < ApplicationController
   def new
+    @deal = Deal.new
   end
 
   def create
+    @deal = Deal.new(deal_params)
+    if @deal.save
+      redirect_to root_path, notice: "契約を提携しました"
+    else
+      render :new
   end
+end
+  private 
+
+  def deal_params
+    params.require(:deal).permit(:time, :money)
+  end 
 end
